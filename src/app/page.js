ab080f882337a3a0a3d9a75062a5bd504cff433f
AkashChaudhary93 <akashchoudhary9368.com>
2025-11-25 22:50:01 +0530

    routing change

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,14 @@
+"use client";
+
+import { use, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+
 export default function AboutPage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/teacher/login");
+  }, [router]);
   return (
     <main>
       <h1>About Page</h1>
